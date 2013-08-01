32009fb256d06d3f1c780ba61b5e460dee811025
Andrew Hollenbach <hollenbach.andrew@gmail.com>
2013-08-01 17:01:37 -0400

    Initial Commit

---
+++ js/main.js
@@ -0,0 +1,6 @@
+window.addEventListener("load", function() {
+	//Util.setCanvasSize("windowed");
+    document.getElementById("button").addEventListener("click", function() {
+        initGame();
+    });
+});
